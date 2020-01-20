ca4ad2dbba7ebf2ef1aff5050d569486e9fb51b5
Yota Toyama <raviqqe@gmail.com>
2020-01-20 08:47:07 -0800

    Refactor errors

--- ssf-llvm/src/error.rs
+++ ssf-llvm/src/error.rs
@@ -3,11 +3,10 @@ use std::fmt::Display;
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum CompileError {
-    AnalysisError(ssf::AnalysisError),
-    InvalidTypeIndex,
-    LlvmError(String),
+    Analysis(ssf::AnalysisError),
+    Llvm(String),
+    TypeCheck(ssf::TypeCheckError),
     VariableNotFound,
-    Verification,
 }
 
 impl Display for CompileError {
@@ -19,19 +18,19 @@ impl Display for CompileError {
 impl Error for CompileError {}
 
 impl From<ssf::TypeCheckError> for CompileError {
-    fn from(_: ssf::TypeCheckError) -> Self {
-        CompileError::Verification
+    fn from(error: ssf::TypeCheckError) -> Self {
+        CompileError::TypeCheck(error)
     }
 }
 
 impl From<ssf::AnalysisError> for CompileError {
     fn from(error: ssf::AnalysisError) -> Self {
-        Self::AnalysisError(error)
+        Self::Analysis(error)
     }
 }
 
 impl From<inkwell::support::LLVMString> for CompileError {
     fn from(string: inkwell::support::LLVMString) -> Self {
-        Self::LlvmError(string.to_string())
+        Self::Llvm(string.to_string())
     }
 }
