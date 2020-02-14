3dac2ad99c564741e321716c2cdcdd7f46a80fd1
Yota Toyama <raviqqe@gmail.com>
2020-02-13 20:02:36 -0800

    Fix canonicalize function scope

--- ssf/src/types/canonicalize/mod.rs
+++ ssf/src/types/canonicalize/mod.rs
@@ -4,6 +4,6 @@ mod type_equality_checker;
 use crate::types::Type;
 use type_canonicalizer::TypeCanonicalizer;
 
-pub fn canonicalize(type_: &Type) -> Type {
+pub(crate) fn canonicalize(type_: &Type) -> Type {
     TypeCanonicalizer::new().canonicalize(type_)
 }
