1dc8b6b197e763eba248b64c86485a89087a81c4
Yota Toyama <raviqqe@gmail.com>
2020-02-18 22:54:08 -0800

    Refactor get_any_type_enum_undef

--- ssf-llvm/src/utilities.rs
+++ ssf-llvm/src/utilities.rs
@@ -1,14 +1,14 @@
-pub fn get_any_type_enum_undef<'a, 'c>(
+pub fn get_any_type_undef<'a, 'c>(
     type_: &'a inkwell::types::AnyTypeEnum<'c>,
-) -> Option<Box<dyn 'a + inkwell::values::BasicValue<'c>>> {
+) -> Option<inkwell::values::BasicValueEnum<'c>> {
     match type_ {
-        inkwell::types::AnyTypeEnum::ArrayType(type_) => Some(Box::new(type_.get_undef())),
-        inkwell::types::AnyTypeEnum::IntType(type_) => Some(Box::new(type_.get_undef())),
-        inkwell::types::AnyTypeEnum::FloatType(type_) => Some(Box::new(type_.get_undef())),
+        inkwell::types::AnyTypeEnum::ArrayType(type_) => Some(type_.get_undef().into()),
+        inkwell::types::AnyTypeEnum::IntType(type_) => Some(type_.get_undef().into()),
+        inkwell::types::AnyTypeEnum::FloatType(type_) => Some(type_.get_undef().into()),
         inkwell::types::AnyTypeEnum::FunctionType(_) => None,
-        inkwell::types::AnyTypeEnum::PointerType(type_) => Some(Box::new(type_.get_undef())),
-        inkwell::types::AnyTypeEnum::StructType(type_) => Some(Box::new(type_.get_undef())),
-        inkwell::types::AnyTypeEnum::VectorType(type_) => Some(Box::new(type_.get_undef())),
+        inkwell::types::AnyTypeEnum::PointerType(type_) => Some(type_.get_undef().into()),
+        inkwell::types::AnyTypeEnum::StructType(type_) => Some(type_.get_undef().into()),
+        inkwell::types::AnyTypeEnum::VectorType(type_) => Some(type_.get_undef().into()),
         inkwell::types::AnyTypeEnum::VoidType(_) => None,
     }
 }
