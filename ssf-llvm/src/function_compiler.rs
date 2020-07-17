c650c2d18ebeaf05880c6483e7ee2c4a103a242e
Yota Toyama <raviqqe@gmail.com>
2020-07-17 00:44:07 +0000

    Cache thunk results

--- ssf-llvm/src/function_compiler.rs
+++ ssf-llvm/src/function_compiler.rs
@@ -2,6 +2,7 @@ use super::compile_configuration::CompileConfiguration;
 use super::error::CompileError;
 use super::expression_compiler::ExpressionCompiler;
 use super::type_compiler::TypeCompiler;
+use inkwell::types::BasicType;
 use std::collections::HashMap;
 
 pub struct FunctionCompiler<'c, 'm, 't, 'v> {
@@ -97,16 +98,92 @@ impl<'c, 'm, 't, 'v> FunctionCompiler<'c, 'm, 't, 'v> {
             self.type_compiler,
             self.compile_configuration,
         );
-        builder.build_return(Some(
-            &expression_compiler.compile(&function_definition.body(), &variables)?,
-        ));
+
+        let result = expression_compiler.compile(&function_definition.body(), &variables)?;
+
+        if function_definition.arguments().is_empty() {
+            // TODO Make this thunk update thread-safe.
+            builder.build_store(
+                builder
+                    .build_bitcast(
+                        environment,
+                        result.get_type().ptr_type(inkwell::AddressSpace::Generic),
+                        "",
+                    )
+                    .into_pointer_value(),
+                result,
+            );
+
+            builder.build_store(
+                unsafe {
+                    builder.build_gep(
+                        builder
+                            .build_bitcast(
+                                environment,
+                                closure_type.get_field_types()[0]
+                                    .ptr_type(inkwell::AddressSpace::Generic),
+                                "",
+                            )
+                            .into_pointer_value(),
+                        &[self.context.i64_type().const_int(-1i64 as u64, true)],
+                        "",
+                    )
+                },
+                self.compile_normal_thunk_entry(function_definition)
+                    .as_global_value()
+                    .as_pointer_value(),
+            );
+        }
+
+        builder.build_return(Some(&result));
 
         entry_function.verify(true);
 
         Ok(entry_function)
     }
 
+    fn compile_normal_thunk_entry(
+        &self,
+        function_definition: &ssf::ir::FunctionDefinition,
+    ) -> inkwell::values::FunctionValue {
+        let closure_type = self.type_compiler.compile_closure(function_definition);
+        let entry_function_type = closure_type.get_field_types()[0]
+            .into_pointer_type()
+            .get_element_type()
+            .into_function_type();
+
+        let entry_function = self.module.add_function(
+            &Self::generate_normal_thunk_entry_name(function_definition.name()),
+            entry_function_type,
+            None,
+        );
+
+        let builder = self.context.create_builder();
+        builder.position_at_end(self.context.append_basic_block(entry_function, "entry"));
+
+        let environment = builder
+            .build_bitcast(
+                entry_function.get_params()[0],
+                entry_function_type
+                    .get_return_type()
+                    .unwrap()
+                    .ptr_type(inkwell::AddressSpace::Generic),
+                "",
+            )
+            .into_pointer_value();
+
+        builder.build_return(Some(&builder.build_load(environment, "")));
+
+        entry_function.verify(true);
+
+        entry_function
+    }
+
     fn generate_closure_entry_name(name: &str) -> String {
         [name, ".$entry"].concat()
+    }
+
+    fn generate_normal_thunk_entry_name(name: &str) -> String {
+        [name, ".$entry.normal"].concat()
     }
 }
