990c276311a8542c53ceac121844cad29d495b98
Yota Toyama <raviqqe@gmail.com>
2020-11-01 23:14:16 +0000

    Add arguments and last_result methods

--- ssf/src/types/function.rs
+++ ssf/src/types/function.rs
@@ -19,8 +19,30 @@ impl Function {
         &self.argument
     }
 
+    pub fn arguments(&self) -> impl IntoIterator<Item = &Type> {
+        let mut arguments = vec![self.argument()];
+        let mut type_ = self;
+
+        while let Type::Function(function) = type_.result() {
+            arguments.push(function.argument());
+            type_ = function;
+        }
+
+        arguments
+    }
+
     pub fn result(&self) -> &Type {
         &self.result
+    }
+
+    pub fn last_result(&self) -> &Type {
+        let mut type_ = self;
+
+        while let Type::Function(function) = type_.result() {
+            type_ = function;
+        }
+
+        type_.result()
     }
 
     pub fn to_id(&self) -> String {
@@ -69,6 +91,40 @@ mod tests {
     fn result() {
         assert_eq!(
             Function::new(Primitive::Float64, Primitive::Float64).result(),
+            &Primitive::Float64.into()
+        );
+    }
+
+    #[test]
+    fn arguments() {
+        assert_eq!(
+            Function::new(Primitive::Float64, Primitive::Float64,)
+                .arguments()
+                .into_iter()
+                .collect::<Vec<&Type>>(),
+            vec![&Primitive::Float64.into()]
+        );
+
+        assert_eq!(
+            Function::new(
+                Primitive::Float64,
+                Function::new(Primitive::Float64, Primitive::Float64)
+            )
+            .arguments()
+            .into_iter()
+            .collect::<Vec<&Type>>(),
+            vec![&Primitive::Float64.into(), &Primitive::Float64.into()]
+        );
+    }
+
+    #[test]
+    fn last_result() {
+        assert_eq!(
+            Function::new(
+                Primitive::Float64,
+                Function::new(Primitive::Float64, Primitive::Float64)
+            )
+            .last_result(),
             &Primitive::Float64.into()
         );
     }
