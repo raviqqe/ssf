66203d8d6df9697c9ed4199d4f488c2f85d40cab
Yota Toyama <raviqqe@gmail.com>
2021-01-02 21:39:39 +0000

    Add store statements

---
+++ ssc/src/ir/atomic_store.rs
@@ -0,0 +1,25 @@
+use super::expression::Expression;
+use std::sync::Arc;
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct AtomicStore {
+    value: Arc<Expression>,
+    pointer: Arc<Expression>,
+}
+
+impl AtomicStore {
+    pub fn new(value: impl Into<Expression>, pointer: impl Into<Expression>) -> Self {
+        Self {
+            value: value.into().into(),
+            pointer: pointer.into().into(),
+        }
+    }
+
+    pub fn value(&self) -> &Expression {
+        &self.value
+    }
+
+    pub fn pointer(&self) -> &Expression {
+        &self.pointer
+    }
+}
--- ssc/src/ir/statement.rs
+++ ssc/src/ir/statement.rs
@@ -1,12 +1,16 @@
+use super::atomic_store::AtomicStore;
 use super::if_::If;
 use super::return_::Return;
+use super::store::Store;
 use super::switch::Switch;
 use super::variable_definition::VariableDefinition;
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Statement {
+    AtomicStore(AtomicStore),
     If(If),
     Return(Return),
+    Store(Store),
     Switch(Switch),
     Unreachable,
     VariableDefinition(VariableDefinition),
