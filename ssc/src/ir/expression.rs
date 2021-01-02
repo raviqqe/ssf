2e08dd3dc4a3e9a19656054b47a1a11ec657ea76
Yota Toyama <raviqqe@gmail.com>
2021-01-02 21:17:57 +0000

    Add primitive expression

--- ssc/src/ir/expression.rs
+++ ssc/src/ir/expression.rs
@@ -1,9 +1,10 @@
 use super::call::Call;
 use super::constructor::Constructor;
+use super::primitive::Primitive;
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Expression {
     Call(Call),
     Constructor(Constructor),
-    Unreachable,
+    Primitive(Primitive),
 }
---
+++ ssc/src/ir/primitive.rs
@@ -0,0 +1,26 @@
+#[derive(Clone, Debug, PartialEq)]
+pub enum Primitive {
+    Float32(f32),
+    Float64(f64),
+    Integer8(u8),
+    Integer32(u32),
+    Integer64(u64),
+}
+
+impl From<f32> for Primitive {
+    fn from(number: f32) -> Self {
+        Self::Float32(number)
+    }
+}
+
+impl From<f64> for Primitive {
+    fn from(number: f64) -> Self {
+        Self::Float64(number)
+    }
+}
+
+impl From<u64> for Primitive {
+    fn from(number: u64) -> Self {
+        Self::Integer64(number)
+    }
+}
