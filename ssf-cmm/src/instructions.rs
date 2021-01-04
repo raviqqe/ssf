c909ff093ee30ce35779554f75db5bd55e2a6561
Yota Toyama <raviqqe@gmail.com>
2021-01-04 03:12:07 +0000

    Fix instructions module

--- ssf-cmm/src/instructions.rs
+++ ssf-cmm/src/instructions.rs
@@ -1,5 +1,20 @@
+use crate::instruction_result::InstructionResult;
 use crate::names;
 
 pub fn bitcast(argument: InstructionResult, type_: cmm::types::Type) -> InstructionResult {
-    (cmm::ir::Bitcast::new())
+    let name = names::generate_name();
+
+    InstructionResult::new(
+        argument
+            .statements()
+            .iter()
+            .cloned()
+            .chain(vec![cmm::ir::Bitcast::new(
+                argument.variable().clone(),
+                type_,
+                &name,
+            )])
+            .collect(),
+        name,
+    )
 }
