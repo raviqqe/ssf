55ccc8533727ea80a7855d90cf5709dcff966497
Yota Toyama <raviqqe@gmail.com>
2025-08-12 18:36:57 -0700

    Fix

--- ssf-fmm/src/types.rs
+++ ssf-fmm/src/types.rs
@@ -44,7 +44,9 @@ pub fn compile_algebraic(algebraic: &ssf::types::Algebraic) -> fmm::types::Recor
 pub fn compile_constructor_union(algebraic_type: &ssf::types::Algebraic) -> fmm::types::Union {
     fmm::types::Union::new(
         algebraic_type
-            .constructors().values().map(compile_shallow_constructor)
+            .constructors()
+            .values()
+            .map(compile_shallow_constructor)
             .collect(),
     )
 }
